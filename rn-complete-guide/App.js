cf159165d8d050dac7614805e9ad1832958e916f
Luke Lopez <xlucas14@gmail.com>
2020-03-16 14:51:14 -0400

    Added a button and text input with some inline styles

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+        <TextInput
+          placeholder="What gems have you found recently?"
+          style={{
+            borderWidth: 1,
+            borderBottomColor: "black",
+            padding: 10
+          }}
+        />
+        <Button title="+" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {}
 });
