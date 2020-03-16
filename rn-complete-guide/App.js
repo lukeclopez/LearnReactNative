305b679aeecd98d63502094b339afd8cb60f9bd8
Luke Lopez <xlucas14@gmail.com>
2020-03-16 19:39:53 -0400

    Replaced inline styles with stylesheets, added state management

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGem, setEnteredGem] = useState("");
+  const [gems, setGems] = useState([]);
+
+  const handleChange = text => {
+    setEnteredGem(text);
+  };
+
+  const handlePress = () => {
+    // The function that returns an array
+    // guarantees that the latest state
+    // will be included.
+    setGems(curGems => [...curGems, enteredGem]);
+    setEnteredGem("");
+  };
+
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center"
-        }}
-      >
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="What gems have you found recently?"
-          style={{
-            borderWidth: 1,
-            borderBottomColor: "black",
-            padding: 10
-          }}
+          style={styles.input}
+          onChangeText={handleChange}
+          value={enteredGem}
         />
-        <Button title="+" />
+        <Button title="Add" onPress={handlePress} />
       </View>
-      <View></View>
+      <View>
+        {gems.map(g => (
+          <View style={styles.gem}>
+            <Text key={g}>{g}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  screen: { padding: 50 },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderWidth: 1,
+    borderBottomColor: "black",
+    padding: 10
+  },
+  gem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "gray",
+    borderColor: "black",
+    borderWidth: 1
+  }
 });
