4c8f46f86a98aa14512214ab50d486f86402ea61
Luke Lopez <xlucas14@gmail.com>
2020-03-17 17:57:11 -0400

    Added and styled a header

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,26 +1,14 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
+import Header from "./components/Header";
+
 export default function App() {
-  const [text, setText] = useState("Luke Lopez was here!");
-
-  const handlePress = () => {
-    setText("You just pressed the button.");
-  };
-
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title="Change text" onPress={handlePress} />
+    <View>
+      <Header title="Hi" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const styles = StyleSheet.create({});
---
+++ rn-first-app/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Header = ({ title }) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    width: "100%",
+    height: "50%",
+    paddingTop: 50,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  headerTitle: {
+    color: "black",
+    fontSize: 18
+  }
+});
+
+export default Header;
