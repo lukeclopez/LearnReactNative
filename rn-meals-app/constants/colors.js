7d9c54035f34ce7d30f023e897f6fa39dfe1277f
Luke Lopez <xlucas14@gmail.com>
2020-04-04 17:20:09 -0400

    Created new expo project with old content/code included to fix expo/dep errors

---
+++ rn-meals-app/constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+  primaryColor: "#34eb86",
+  accentColor: "#34ebdc",
+};
---
+++ rn-meals-app/screens/Categories.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+
+import { CATEGORIES } from "./../data/dummyData";
+import colors from "../constants/colors";
+
+const Categories = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={props.navigation.navigate("CategoryMeals")}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.screen}>
+      <FlatList numColumns={2} data={CATEGORIES} renderItem={renderGridItem} />
+      <Text>The category screen!</Text>
+      <Button
+        title="Go to meals"
+        onPress={() => {
+          props.navigation.navigate("CategoryMeals");
+        }}
+      />
+    </View>
+  );
+};
+
+Categories.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: colors.primaryColor,
+  },
+  headerTintColor: "white",
+};
+
+export default Categories;
+
+const styles = StyleSheet.create({
+  gridItem: { flex: 1, margin: 15, heigh: 150 },
+});
