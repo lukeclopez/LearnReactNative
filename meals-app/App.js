98424fee4601c9d4e08f1375d2b6357931b2e15d
Luke Lopez <xlucas14@gmail.com>
2020-04-07 08:44:41 -0400

    Enable screens for better performance

--- meals-app/App.js
+++ meals-app/App.js
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import { Text, View } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
+import { enableScreens } from "react-native-screens";
 
 import MealsNavigator from "./navigation/Meals";
+
+// Makes app more efficient
+enableScreens();
 
 const fetchFonts = () => {
   return Font.loadAsync({
