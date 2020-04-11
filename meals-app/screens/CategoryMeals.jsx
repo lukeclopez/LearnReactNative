c8c56a38dda115e73faa9b94fbd2779d21bd89a2
Luke Lopez <xlucas14@gmail.com>
2020-04-11 09:37:04 -0400

    Use FlatList to display meals

--- meals-app/screens/CategoryMeals.jsx
+++ meals-app/screens/CategoryMeals.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, FlatList } from "react-native";
 
-import { CATEGORIES } from "../data/dummyData";
+import { CATEGORIES, MEALS } from "../data/dummyData";
 
 const CategoryMeals = (props) => {
   const catId = props.navigation.getParam("categoryId");
-  const selectedCategory = CATEGORIES.find((cat) => cat.id == catId);
+  const displayedMeals = MEALS.filter((e) => e.categoryIds.indexOf(catId) >= 0);
+
+  const renderMealItem = (itemData) => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.screen}>
-      <Text>The category meals screen!</Text>
-      <Text>{selectedCategory.title}</Text>
+      <View>
+        <FlatList data={displayedMeals} renderItem={renderMealItem} />
+      </View>
       <Button
         title="Meal Details"
         onPress={() => {
